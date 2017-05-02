26f6e4556678cd2ef5576a11f75969586534f80a
MUstaf Eli <elimustaf@gmail.com>
2017-05-01 23:09:54 -0700

    Give the app and Backend

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,27 +1,52 @@
 # "Database code" for the DB Forum.
 
-import datetime
-
-# POSTS = [("This is the first post.", datetime.datetime.now())]
-
-## Database connection
-DB = []
+import psycopg2
 
 # Get posts from database.
-def GetALLPosts():
-  """Return all posts from the 'database', most recent first."""
+def GetAllPosts():
   DB = psycopg2.connect("dbname=forum")
-  posts = [{'content': str(row[1]),
-  'time': str(row[0])} for row in DB]
-  posts.sort(key=lambda row: row['time'], reverse = True)
+  c = DB.cursor()
+  c.execute("SELECT time, content FROM posts ORDER BY time DESC")
+  posts =({'content': str(row[1], 'time' str(row[0])}
+           for row in c.fetchall()0
+  DB.close()
   return posts
-#  return reversed(POSTS)
 
 # Add a post to the database.
 def AddPost(content):
-  """Add a post to the 'database' with the current timestamp."""
-  t = time.strftime('%', time.localtime())
-  DB.append((t, content))
- # POSTS.append((content, datetime.datetime.now()))
+  DB = psycopg2.connect("dbname=forum")
+  c = DB.cursor()
+  c.execute("INSERT INTO posts(content) VALUES ('%s')" % content)
+  DB.commit()
+  DB.close()
+  
+
+
+
+
+
+
+
+
+
+##import datetime
+# POSTS = [("This is the first post.", datetime.datetime.now())]
+## Database connection
+## DB = []
+### Get posts from database.
+##def GetALLPosts():
+##  """Return all posts from the 'database', most recent first."""
+##  posts = [{'content': str(row[1]),
+##  'time': str(row[0])} for row in DB]
+##  posts.sort(key=lambda row: row['time'], reverse = True)
+##  return posts
+##  return reversed(POSTS)
+##
+### Add a post to the database.
+##def AddPost(content):
+##  """Add a post to the 'database' with the current timestamp."""
+##  t = time.strftime('%', time.localtime())
+##  DB.append((t, content))
+## # POSTS.append((content, datetime.datetime.now()))
 
 
